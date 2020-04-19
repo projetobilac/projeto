0f69e4bbc76a7af39858f9c224609795ebf7641e
Chaiana <projetobilac2s@gmail.com>
2020-04-19 03:23:07 -0300

    criação da tela de login

--- app/src/main/java/com/example/projetoreciclasanja/TelaLogin.java
+++ app/src/main/java/com/example/projetoreciclasanja/TelaLogin.java
@@ -3,12 +3,15 @@ package com.example.projetoreciclasanja;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class TelaLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_dashboard);
     }
 }
