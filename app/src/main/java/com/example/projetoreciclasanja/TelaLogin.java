5490562135a916e9c340efdc82a3e88f1faf1625
projetobilac <63132560+projetobilac@users.noreply.github.com>
2020-04-30 21:53:50 -0300

    Ajuste da tela de Cadastro e Ação botao entrar

--- app/src/main/java/com/example/projetoreciclasanja/TelaLogin.java
+++ app/src/main/java/com/example/projetoreciclasanja/TelaLogin.java
@@ -7,6 +7,7 @@ import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.view.WindowManager;
@@ -64,5 +65,13 @@ public class TelaLogin extends AppCompatActivity {
 
             }
         });
+
+            login_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(TelaLogin.this, TelaTeste.class);
+                startActivity(it);
+            }
+        });
     }
+
 }
