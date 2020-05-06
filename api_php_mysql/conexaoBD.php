e1cdf6442b935c0eac6c48f69701179a24b19ba2
projetobilac <63132560+projetobilac@users.noreply.github.com>
2020-05-06 18:36:33 -0300

    Create conexaoBD.php

---
+++ api_php_mysql/conexaoBD.php
@@ -0,0 +1,11 @@
+<?php
+
+//Definindo constantes
+define('HOST','localhost');
+define('USER','id13480313_recicla');
+define('PASS','m53*K<mMpp]WsU$H');
+define('DB','id13480313_reciclasanja');
+
+//Conectando o banco de dados
+$con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+?>
